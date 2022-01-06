3899d2aebce72468087e950d8336cce8f5aac939
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-01-06 14:39:09 +0100

    Use env var `HTTP_SERVER_PORT` for setting the http port

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,9 @@
 import { CommercetoolsMock } from './index'
 
 const instance = new CommercetoolsMock()
-instance.runServer(3000)
+
+let port = 3000
+
+if (process.env.HTTP_SERVER_PORT) port = parseInt(process.env.HTTP_SERVER_PORT)
+
+instance.runServer(port)
