3b2ed737b8f7c0014ac91ff5d62c423ffdc9c8c7
Mike de Bock <mike.debock@masterwebber.nl>
2022-02-23 12:21:23 +0100

    implemented cart update actions

--- src/services/cart.test.ts
+++ src/services/cart.test.ts
@@ -1,6 +1,7 @@
-import { Cart } from '@commercetools/platform-sdk'
+import { Address, Cart } from '@commercetools/platform-sdk'
 import supertest from 'supertest'
 import { CommercetoolsMock } from '../index'
+import assert from 'assert'
 
 describe('Carts Query', () => {
   const ctMock = new CommercetoolsMock()
@@ -67,5 +68,110 @@ describe('Carts Query', () => {
     expect(myCart.custom?.type.id).not.toBeUndefined()
     expect(myCart.custom?.type.id).toBe(myCart.custom?.type.obj?.id)
     expect(myCart.custom?.type.obj?.description?.en).toBe('Test Type')
+  })
+})
+
+describe('Order Update Actions', () => {
+  const ctMock = new CommercetoolsMock()
+  let cart: Cart | undefined
+
+  beforeEach(async () => {
+    let response = await supertest(ctMock.app).post('/dummy/carts').send({
+      currency: 'EUR',
+    })
+    expect(response.status).toBe(201)
+    cart = response.body
+  })
+
+  test('no update', async () => {
+    assert(cart, 'cart not created')
+
+    const response = await supertest(ctMock.app)
+      .post(`/dummy/carts/${cart.id}`)
+      .send({
+        version: 1,
+        actions: [{ action: 'setLocale', locale: 'nl-NL' }],
+      })
+    expect(response.status).toBe(200)
+    expect(response.body.version).toBe(2)
+    expect(response.body.locale).toBe('nl-NL')
+
+    const responseAgain = await supertest(ctMock.app)
+      .post(`/dummy/carts/${cart.id}`)
+      .send({
+        version: 2,
+        actions: [{ action: 'setLocale', locale: 'nl-NL' }],
+      })
+    expect(responseAgain.status).toBe(200)
+    expect(responseAgain.body.version).toBe(2)
+    expect(responseAgain.body.locale).toBe('nl-NL')
+  })
+
+  test('setBillingAddress', async () => {
+    assert(cart, 'cart not created')
+
+    const address: Address = {
+      streetName: 'Street name',
+      city: 'Utrecht',
+      country: 'NL',
+    }
+
+    const response = await supertest(ctMock.app)
+      .post(`/dummy/carts/${cart.id}`)
+      .send({
+        version: 1,
+        actions: [{ action: 'setBillingAddress', address }],
+      })
+    expect(response.status).toBe(200)
+    expect(response.body.version).toBe(2)
+    expect(response.body.billingAddress).toEqual(address)
+  })
+
+  test('setCountry', async () => {
+    assert(cart, 'cart not created')
+
+    const response = await supertest(ctMock.app)
+      .post(`/dummy/carts/${cart.id}`)
+      .send({
+        version: 1,
+        actions: [{ action: 'setCountry', country: 'NL' }],
+      })
+    expect(response.status).toBe(200)
+    expect(response.body.version).toBe(2)
+    expect(response.body.country).toBe('NL')
+  })
+
+  test('setCustomerEmail', async () => {
+    assert(cart, 'cart not created')
+
+    const response = await supertest(ctMock.app)
+      .post(`/dummy/carts/${cart.id}`)
+      .send({
+        version: 1,
+        actions: [{ action: 'setCustomerEmail', email: 'john@doe.com' }],
+      })
+    expect(response.status).toBe(200)
+    expect(response.body.version).toBe(2)
+    expect(response.body.customerEmail).toBe('john@doe.com')
+  })
+
+  test('setShippingAddress', async () => {
+    assert(cart, 'cart not created')
+
+    const address: Address = {
+      streetName: 'Street name',
+      city: 'Utrecht',
+      country: 'NL',
+    }
+
+    const response = await supertest(ctMock.app)
+      .post(`/dummy/carts/${cart.id}`)
+      .send({
+        version: 1,
+        actions: [{ action: 'setShippingAddress', address }],
+      })
+    expect(response.status).toBe(200)
+    expect(response.body.version).toBe(2)
+    expect(response.body.shippingAddress).toEqual(address)
   })
 })
