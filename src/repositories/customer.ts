797265dd9eebdeb9511de3ea59c405740b3b27f1
bramkaashoek <b.kaashoek@labdigital.nl>
2022-05-03 17:27:50 +0200

    remove incorrect comment

--- src/repositories/customer.ts
+++ src/repositories/customer.ts
@@ -22,7 +22,6 @@ export class CustomerRepository extends AbstractResourceRepository {
     return resource
   }
   getMe(projectKey: string): Customer | undefined {
-    // Get first active cart
     const results = this._storage.query(projectKey, this.getTypeId(), {}) // grab the first customer you can find
     if (results.count > 0) {
       return results.results[0] as Customer
