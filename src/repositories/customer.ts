c3e860f9a29258331fdbc7252e89c976c5dc3b4f
willstaff <william.staff@frasers.group>
2022-05-16 13:15:35 +0100

    feat: add customer changeEmail action

--- src/repositories/customer.ts
+++ src/repositories/customer.ts
@@ -1,32 +1,44 @@
 import {
   Customer,
+  CustomerChangeEmailAction,
   CustomerDraft,
   ReferenceTypeId,
 } from '@commercetools/platform-sdk'
+import { Writable } from 'types'
 import { getBaseResourceProperties } from '../helpers'
 import { AbstractResourceRepository } from './abstract'
 
 export class CustomerRepository extends AbstractResourceRepository {
-  getTypeId(): ReferenceTypeId {
-    return 'customer'
-  }
-  create(projectKey: string, draft: CustomerDraft): Customer {
-    const resource: Customer = {
-      ...getBaseResourceProperties(),
-      email: draft.email,
-      password: Buffer.from(draft.password).toString('base64'),
-      isEmailVerified: draft.isEmailVerified || false,
-      addresses: [],
-    }
-    this.save(projectKey, resource)
-    return resource
-  }
-  getMe(projectKey: string): Customer | undefined {
-    const results = this._storage.query(projectKey, this.getTypeId(), {}) // grab the first customer you can find
-    if (results.count > 0) {
-      return results.results[0] as Customer
-    }
+         getTypeId(): ReferenceTypeId {
+           return 'customer'
+         }
+         create(projectKey: string, draft: CustomerDraft): Customer {
+           const resource: Customer = {
+             ...getBaseResourceProperties(),
+             email: draft.email,
+             password: Buffer.from(draft.password).toString('base64'),
+             isEmailVerified: draft.isEmailVerified || false,
+             addresses: [],
+           }
+           this.save(projectKey, resource)
+           return resource
+         }
+         getMe(projectKey: string): Customer | undefined {
+           const results = this._storage.query(projectKey, this.getTypeId(), {}) // grab the first customer you can find
+           if (results.count > 0) {
+             return results.results[0] as Customer
+           }
 
-    return
-  }
-}
+           return
+         }
+
+         actions = {
+           changeEmail: (
+             _projectKey: string,
+             resource: Writable<Customer>,
+             { email }: CustomerChangeEmailAction
+           ) => {
+             resource.email = email
+           },
+         }
+       }
