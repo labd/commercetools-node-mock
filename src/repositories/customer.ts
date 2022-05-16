707f058d8e6df0cf03aefa0f589eb4cbfb107261
willstaff <william.staff@frasers.group>
2022-05-16 15:23:57 +0100

    fix: weird spacing

--- src/repositories/customer.ts
+++ src/repositories/customer.ts
@@ -9,36 +9,36 @@ import { getBaseResourceProperties } from '../helpers'
 import { AbstractResourceRepository } from './abstract'
 
 export class CustomerRepository extends AbstractResourceRepository {
-         getTypeId(): ReferenceTypeId {
-           return 'customer'
-         }
-         create(projectKey: string, draft: CustomerDraft): Customer {
-           const resource: Customer = {
-             ...getBaseResourceProperties(),
-             email: draft.email,
-             password: Buffer.from(draft.password).toString('base64'),
-             isEmailVerified: draft.isEmailVerified || false,
-             addresses: [],
-           }
-           this.save(projectKey, resource)
-           return resource
-         }
-         getMe(projectKey: string): Customer | undefined {
-           const results = this._storage.query(projectKey, this.getTypeId(), {}) // grab the first customer you can find
-           if (results.count > 0) {
-             return results.results[0] as Customer
-           }
+  getTypeId(): ReferenceTypeId {
+    return 'customer'
+  }
+  create(projectKey: string, draft: CustomerDraft): Customer {
+    const resource: Customer = {
+      ...getBaseResourceProperties(),
+      email: draft.email,
+      password: Buffer.from(draft.password).toString('base64'),
+      isEmailVerified: draft.isEmailVerified || false,
+      addresses: [],
+    }
+    this.save(projectKey, resource)
+    return resource
+  }
+  getMe(projectKey: string): Customer | undefined {
+    const results = this._storage.query(projectKey, this.getTypeId(), {}) // grab the first customer you can find
+    if (results.count > 0) {
+      return results.results[0] as Customer
+    }
 
-           return
-         }
+    return
+  }
 
-         actions = {
-           changeEmail: (
-             _projectKey: string,
-             resource: Writable<Customer>,
-             { email }: CustomerChangeEmailAction
-           ) => {
-             resource.email = email
-           },
-         }
-       }
+  actions = {
+    changeEmail: (
+      _projectKey: string,
+      resource: Writable<Customer>,
+      { email }: CustomerChangeEmailAction
+    ) => {
+      resource.email = email
+    },
+  }
+}
