2d6539117ecacc07f4f403fcb271365cac5be111
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-06-24 15:00:48 +0200

    Add support for custom fields on customer-group resource

--- src/repositories/customer-group.ts
+++ src/repositories/customer-group.ts
@@ -2,12 +2,15 @@ import {
   CustomerGroup,
   CustomerGroupChangeNameAction,
   CustomerGroupDraft,
+  CustomerGroupSetCustomFieldAction,
+  CustomerGroupSetCustomTypeAction,
   CustomerGroupSetKeyAction,
   ReferenceTypeId,
 } from '@commercetools/platform-sdk'
 import { Writable } from 'types'
 import { getBaseResourceProperties } from '../helpers'
 import { AbstractResourceRepository, RepositoryContext } from './abstract'
+import { createCustomFields } from './helpers'
 
 export class CustomerGroupRepository extends AbstractResourceRepository {
   getTypeId(): ReferenceTypeId {
@@ -18,6 +21,11 @@ export class CustomerGroupRepository extends AbstractResourceRepository {
       ...getBaseResourceProperties(),
       key: draft.key,
       name: draft.groupName,
+      custom: createCustomFields(
+        draft.custom,
+        context.projectKey,
+        this._storage
+      ),
     }
     this.save(context, resource)
     return resource
@@ -37,6 +45,35 @@ export class CustomerGroupRepository extends AbstractResourceRepository {
       { name }: CustomerGroupChangeNameAction
     ) => {
       resource.name = name
+    },
+    setCustomType: (
+      context: RepositoryContext,
+      resource: Writable<CustomerGroup>,
+      { type, fields }: CustomerGroupSetCustomTypeAction
+    ) => {
+      if (type) {
+        resource.custom = createCustomFields(
+          { type, fields },
+          context.projectKey,
+          this._storage
+        )
+      } else {
+        resource.custom = undefined
+      }
+    },
+    setCustomField: (
+      context: RepositoryContext,
+      resource: Writable<CustomerGroup>,
+      { name, value }: CustomerGroupSetCustomFieldAction
+    ) => {
+      if (!resource.custom) {
+        return
+      }
+      if (value === null) {
+        delete resource.custom.fields[name]
+      } else {
+        resource.custom.fields[name] = value
+      }
     },
   }
 }
