1e68101421f009fca4a30d950a46a69e49759175
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-10-08 11:18:00 +0200

    Fix product setAttribute handling for staged products

--- src/repositories/product.ts
+++ src/repositories/product.ts
@@ -6,17 +6,17 @@ import {
   ProductDraft,
   ProductPublishAction,
   ProductSetAttributeAction,
-  ProductType,
   ProductTypeReference,
   ProductVariant,
   ProductVariantDraft,
   ReferenceTypeId,
 } from '@commercetools/platform-sdk'
 import { v4 as uuidv4 } from 'uuid'
-import { getBaseResourceProperties } from '../helpers'
+import { getBaseResourceProperties, cloneObject } from '../helpers'
 import { AbstractResourceRepository, RepositoryContext } from './abstract'
 import { Writable } from '../types'
 import { getReferenceFromResourceIdentifier } from './helpers'
+import deepEqual from 'deep-equal'
 
 export class ProductRepository extends AbstractResourceRepository {
   getTypeId(): ReferenceTypeId {
@@ -52,9 +52,7 @@ export class ProductRepository extends AbstractResourceRepository {
       categories: [],
       masterVariant: variantFromDraft(1, draft.masterVariant),
       variants:
-        draft.variants?.map((variant, index) => {
-          return variantFromDraft(index + 2, variant)
-        }) ?? [],
+        draft.variants?.map((variant, index) => variantFromDraft(index + 2, variant)) ?? [],
 
       // @ts-ignore
       searchKeywords: draft.searchKeywords,
@@ -66,9 +64,8 @@ export class ProductRepository extends AbstractResourceRepository {
       masterData: {
         // @ts-ignore
         current: draft.publish ? productData : undefined,
-        // @ts-ignore
-        staged: draft.publish ? undefined : productData,
-        hasStagedChanges: draft.publish ?? true,
+        staged: productData,
+        hasStagedChanges: false,
         published: draft.publish ?? false,
       },
     }
@@ -84,61 +81,65 @@ export class ProductRepository extends AbstractResourceRepository {
       resource: Writable<Product>,
       { scope }: ProductPublishAction
     ) => {
-      if (resource.masterData.staged) {
-        resource.masterData.current = resource.masterData.staged
-        // @ts-ignore
-        resource.masterData.staged = undefined
-      }
-      resource.masterData.hasStagedChanges = false
+      resource.masterData.current = getStagedData(resource)
       resource.masterData.published = true
+      checkForStagedChanges(resource)
     },
     setAttribute: (
       context: RepositoryContext,
       resource: Writable<Product>,
       { variantId, sku, name, value, staged }: ProductSetAttributeAction
     ) => {
-      const isStaged = staged !== undefined ? staged : false
-      const productData = getProductData(resource, isStaged)
-      const { variant, isMasterVariant, variantIndex } = getVariant(
-        productData,
-        variantId,
-        sku
-      )
-      if (!variant) {
-        throw new Error(
-          `Variant with id ${variantId} or sku ${sku} not found on product ${resource.id}`
+
+      const setAttr = (data: Writable<ProductData>) => {
+        const { variant, isMasterVariant, variantIndex } = getVariant(
+          data,
+          variantId,
+          sku
         )
-      }
-
-      if (!variant.attributes) {
-        variant.attributes = []
-      }
-
-      const existingAttr = variant.attributes.find(attr => attr.name === name)
-      if (existingAttr) {
-        existingAttr.value = value
-      } else {
-        variant.attributes.push({
-          name,
-          value,
-        })
-      }
-      if (isStaged) {
-        resource.masterData.staged = productData
-        if (isMasterVariant) {
-          resource.masterData.staged.masterVariant = variant
-        } else {
-          resource.masterData.staged.variants[variantIndex] = variant
+        if (!variant) {
+          throw new Error(
+            `Variant with id ${variantId} or sku ${sku} not found on product ${resource.id}`
+          )
         }
-        resource.masterData.hasStagedChanges = true
-      } else {
-        resource.masterData.current = productData
-        if (isMasterVariant) {
-          resource.masterData.current.masterVariant = variant
+
+        if (!variant.attributes) {
+          variant.attributes = []
+        }
+
+        const existingAttr = variant.attributes.find(attr => attr.name === name)
+        if (existingAttr) {
+          existingAttr.value = value
         } else {
-          resource.masterData.current.variants[variantIndex] = variant
+          variant.attributes.push({
+            name,
+            value,
+          })
+        }
+        if (isMasterVariant) {
+          data.masterVariant = variant
+        } else {
+        data.variants[variantIndex] = variant
         }
       }
+
+      // If true, only the staged Attribute is set. If false, both current and
+      // staged Attribute is set.  Default is true
+      const isStaged = staged !== undefined ? staged : true
+
+      // Write the attribute to the staged data
+      const stagedData = getStagedData(resource)
+      setAttr(stagedData)
+
+      // Also write to published data is isStaged = false
+      // if isStaged is false we set the attribute on both the staged and
+      // published data.
+      if (!isStaged) {
+        setAttr(resource.masterData.current)
+      }
+      checkForStagedChanges(resource)
+
+      return resource
     },
     // 'setKey': () => {},
     // 'changeName': () => {},
@@ -186,12 +187,29 @@ export class ProductRepository extends AbstractResourceRepository {
   }
 }
 
-const getProductData = (product: Product, staged: boolean) => {
-  if (!staged && product.masterData.current) {
-    return product.masterData.current
+
+// Return staged data. If no staged data is set we copy the current data
+const getStagedData = (product: Writable<Product>) => {
+  if (!product.masterData.staged) {
+    product.masterData.staged = cloneObject(product.masterData.current)
   }
   return product.masterData.staged
 }
+
+// Check if the product still has staged data that is different from the
+// current data.
+const checkForStagedChanges = (product: Writable<Product>) => {
+  if (!product.masterData.staged) {
+    product.masterData.staged = getStagedData(product)
+  }
+
+  if (deepEqual(product.masterData.current, product.masterData.staged)) {
+    product.masterData.hasStagedChanges = false
+  } else {
+    product.masterData.hasStagedChanges = true
+  }
+}
+
 
 interface VariantResult {
   variant: Writable<ProductVariant> | undefined
@@ -229,19 +247,16 @@ const getVariant = (
 const variantFromDraft = (
   variantId: number,
   variant: ProductVariantDraft
-): ProductVariant => {
-  return {
+): ProductVariant => ({
     id: variantId,
     sku: variant?.sku,
     attributes: variant?.attributes ?? [],
     prices: variant?.prices?.map(priceFromDraft),
     assets: [],
     images: [],
-  }
-}
+  })
 
-const priceFromDraft = (draft: PriceDraft): Price => {
-  return {
+const priceFromDraft = (draft: PriceDraft): Price => ({
     id: uuidv4(),
     value: {
       currencyCode: draft.value.currencyCode,
@@ -249,5 +264,4 @@ const priceFromDraft = (draft: PriceDraft): Price => {
       fractionDigits: 2,
       type: 'centPrecision',
     },
-  }
-}
+  })
