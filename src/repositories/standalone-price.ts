009d41ab96cc7114dc56f80809f0c62e4083bb64
Balachandra Pai <balachandra.pai@goflink.com>
2023-03-13 17:59:36 +0100

    Adding update actions

--- src/repositories/standalone-price.ts
+++ src/repositories/standalone-price.ts
@@ -1,11 +1,13 @@
 import {
-  Review,
   StandalonePrice,
-  StandalonePriceUpdateAction,
+  StandalonePriceChangeActiveAction,
+  StandalonePriceChangeValueAction,
+  StandalonePriceSetDiscountedPriceAction,
 } from '@commercetools/platform-sdk'
 import { getBaseResourceProperties } from '../helpers'
 import { Writable } from '../types'
 import { AbstractResourceRepository, RepositoryContext } from './abstract'
+import { createTypedMoney } from './helpers'
 
 export class StandAlonePriceRepository extends AbstractResourceRepository<'standalone-price'> {
   getTypeId() {
@@ -17,20 +19,42 @@ export class StandAlonePriceRepository extends AbstractResourceRepository<'stand
       ...getBaseResourceProperties(),
       active: draft.active,
       sku: draft.sku,
-      value: draft.value,
+      value: createTypedMoney(draft.value),
+      country: draft.country,
+      channel: draft.channel,
     }
     this.saveNew(context, resource)
     return resource
   }
 
-  actions: Partial<
-    Record<
-      StandalonePriceUpdateAction['action'],
-      (
-        context: RepositoryContext,
-        resource: Writable<Review>,
-        action: any
-      ) => void
-    >
-  > = {}
+  actions = {
+    setActive: (
+      context: RepositoryContext,
+      resource: Writable<StandalonePrice>,
+      action: StandalonePriceChangeActiveAction
+    ) => {
+      resource.active = action.active
+    },
+    changeValue: (
+      context: RepositoryContext,
+      resource: Writable<StandalonePrice>,
+      action: StandalonePriceChangeValueAction
+    ) => {
+      resource.value = createTypedMoney(action.value)
+    },
+    setDiscountedPrice: ( 
+      context: RepositoryContext,
+      resource: Writable<StandalonePrice>,
+      action: StandalonePriceSetDiscountedPriceAction
+    ) => {
+      if (action.discounted) {
+        resource.discounted = {
+          value: createTypedMoney(action.discounted.value),
+          discount: action.discounted.discount,
+        }
+      } else {
+        resource.discounted = undefined
+      }
+    }
+  }
 }
--- src/services/standalone-price.test.ts
+++ src/services/standalone-price.test.ts
@@ -34,4 +34,35 @@ describe('StandalonePrice', () => {
         const response = await supertest(ctMock.app).get(`/dummy/standalone-prices/${createResponse.body.id}`)
         expect(response.status).toBe(200)
     })
+
+    test('Update standalone price', async () => {
+        const draft: StandalonePriceDraft = {
+            sku: 'test-sku',
+            value: {
+                currencyCode: 'EUR',
+                centAmount: 1000,
+            },
+            active: true,
+        }
+
+        const createResponse = await supertest(ctMock.app).post('/dummy/standalone-prices').send(draft)
+
+        const response = await supertest(ctMock.app)
+            .post(`/dummy/standalone-prices/${createResponse.body.id}`)
+            .send({
+                version: createResponse.body.version,
+                actions: [
+                    {
+                        action: 'changeValue',
+                        value: {
+                            currencyCode: 'EUR',
+                            centAmount: 2000,
+                        },
+                    },
+                ],
+            })
+        expect(response.status).toBe(200)
+        expect(response.body.id).toBe(createResponse.body.id)
+        expect(response.body.value.centAmount).toBe(2000)
+    })
 })
