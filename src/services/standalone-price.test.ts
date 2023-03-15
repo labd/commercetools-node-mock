414e91b4876e260a51b46ba28be41ad6be22d1c9
Balachandra Pai <balachandra.pai@goflink.com>
2023-03-15 11:55:09 +0100

    adding tests

--- src/services/standalone-price.test.ts
+++ src/services/standalone-price.test.ts
@@ -4,65 +4,272 @@ import { CommercetoolsMock } from '../index'
 
 const ctMock = new CommercetoolsMock()
 
-describe('StandalonePrice', () => {
-    test('Create standalone price', async () => {
+describe('Standalone price Query', () => {
+    beforeAll(async () => {
         const draft: StandalonePriceDraft = {
-            sku: 'test-sku',
             value: {
+                centAmount: 100,
                 currencyCode: 'EUR',
-                centAmount: 1000,
             },
+            country: 'DE',
+            sku: 'foo',
             active: true,
+            channel: {
+                typeId: 'channel',
+                id: 'bar',
+            },
+            discounted: {
+                value: {
+                    centAmount: 80,
+                    currencyCode: 'EUR',
+                },
+                discount: {
+                    typeId: 'product-discount',
+                    id: 'baz',
+                },
+            },
         }
+        const createResponse = await supertest(ctMock.app)
+            .post('/dummy/standalone-prices')
+            .send(draft)
+        expect(createResponse.status).toEqual(201)
+    })
 
-        const response = await supertest(ctMock.app).post('/dummy/standalone-prices').send(draft)
-        expect(response.status).toBe(201)
+    afterAll(async () => {
+        ctMock.clear()
     })
 
     test('Get standalone price', async () => {
-        const draft: StandalonePriceDraft = {
-            sku: 'test-sku',
-            value: {
-                currencyCode: 'EUR',
-                centAmount: 1000,
-            },
-            active: true,
-        }
+        const response = await supertest(ctMock.app).get('/dummy/standalone-prices?sku=foo')
 
-        const createResponse = await supertest(ctMock.app).post('/dummy/standalone-prices').send(draft)
-
-        const response = await supertest(ctMock.app).get(`/dummy/standalone-prices/${createResponse.body.id}`)
         expect(response.status).toBe(200)
+
+        expect(response.body.results).toEqual([
+            {
+                active: true,
+                channel: {
+                    id: 'bar',
+                    typeId: 'channel',
+                },
+                country: 'DE',
+                createdAt: expect.anything(),
+                discounted: {
+                    discount: {
+                        id: 'baz',
+                        typeId: 'product-discount',
+                    },
+                    value: {
+                        centAmount: 80,
+                        currencyCode: 'EUR',
+                        fractionDigits: 2,
+                        type: 'centPrecision',
+                    },
+                },
+                id: expect.anything(),
+                lastModifiedAt: expect.anything(),
+                sku: 'foo',
+                value: {
+                    centAmount: 100,
+                    currencyCode: 'EUR',
+                    fractionDigits: 2,
+                    type: 'centPrecision',
+                },
+                version: 1,
+            },
+        ])
+    })
+})
+
+describe('Standalone price Actions', () => {
+    let id: string | undefined
+    beforeEach(async () => {
+        const draft: StandalonePriceDraft = {
+            value: {
+                centAmount: 100,
+                currencyCode: 'EUR',
+            },
+            country: 'DE',
+            sku: 'foo',
+            active: true,
+            channel: {
+                typeId: 'channel',
+                id: 'bar',
+            },
+        }
+        const createResponse = await supertest(ctMock.app)
+            .post('/dummy/standalone-prices')
+            .send(draft)
+        expect(createResponse.status).toEqual(201)
+        id = createResponse.body.id
     })
 
-    test('Update standalone price', async () => {
-        const draft: StandalonePriceDraft = {
-            sku: 'test-sku',
-            value: {
-                currencyCode: 'EUR',
-                centAmount: 1000,
-            },
-            active: true,
-        }
+    afterEach(async () => {
+        ctMock.clear()
+    })
 
-        const createResponse = await supertest(ctMock.app).post('/dummy/standalone-prices').send(draft)
-
+    test('changeValue', async () => {
         const response = await supertest(ctMock.app)
-            .post(`/dummy/standalone-prices/${createResponse.body.id}`)
+            .post('/dummy/standalone-prices/' + id)
             .send({
-                version: createResponse.body.version,
+                version: 1,
                 actions: [
                     {
                         action: 'changeValue',
                         value: {
+                            centAmount: 200,
                             currencyCode: 'EUR',
-                            centAmount: 2000,
                         },
                     },
                 ],
             })
+
         expect(response.status).toBe(200)
-        expect(response.body.id).toBe(createResponse.body.id)
-        expect(response.body.value.centAmount).toBe(2000)
+
+        expect(response.body).toEqual({
+            active: true,
+            channel: {
+                id: 'bar',
+                typeId: 'channel',
+            },
+            country: 'DE',
+            createdAt: expect.anything(),
+            id: id,
+            lastModifiedAt: expect.anything(),
+            sku: 'foo',
+            value: {
+                centAmount: 200,
+                currencyCode: 'EUR',
+                fractionDigits: 2,
+                type: 'centPrecision',
+            },
+            version: 2,
+        })
+    })
+
+    test('setActive', async () => {
+        const response = await supertest(ctMock.app)
+            .post('/dummy/standalone-prices/' + id)
+            .send({
+                version: 1,
+                actions: [
+                    {
+                        action: 'setActive',
+                        active: false,
+                    },
+                ],
+            })
+
+        expect(response.status).toBe(200)
+
+        expect(response.body).toEqual({
+            active: false,
+            channel: {
+                id: 'bar',
+                typeId: 'channel',
+            },
+            country: 'DE',
+            createdAt: expect.anything(),
+            id: id,
+            lastModifiedAt: expect.anything(),
+            sku: 'foo',
+            value: {
+                centAmount: 100,
+                currencyCode: 'EUR',
+                fractionDigits: 2,
+                type: 'centPrecision',
+            },
+            version: 2,
+        })
+    })
+
+    test('setDiscounted', async () => {
+        const response = await supertest(ctMock.app)
+            .post('/dummy/standalone-prices/' + id)
+            .send({
+                version: 1,
+                actions: [
+                    {
+                        action: 'setDiscountedPrice',
+                        discounted: {
+                            value: {
+                                centAmount: 80,
+                                currencyCode: 'EUR',
+                            },
+                            discount: {
+                                typeId: 'product-discount',
+                                id: 'baz',
+                            },
+                        },
+                    },
+                ],
+            })
+
+        expect(response.status).toBe(200)
+
+        expect(response.body).toEqual({
+            active: true,
+            channel: {
+                id: 'bar',
+                typeId: 'channel',
+            },
+            country: 'DE',
+            createdAt: expect.anything(),
+            discounted: {
+                discount: {
+                    id: 'baz',
+                    typeId: 'product-discount',
+                },
+                value: {
+                    centAmount: 80,
+                    currencyCode: 'EUR',
+                    fractionDigits: 2,
+                    type: 'centPrecision',
+                },
+            },
+            id: id,
+            lastModifiedAt: expect.anything(),
+            sku: 'foo',
+            value: {
+                centAmount: 100,
+                currencyCode: 'EUR',
+                fractionDigits: 2,
+                type: 'centPrecision',
+            },
+            version: 2,
+        })
+
+        const response2 = await supertest(ctMock.app)
+            .post('/dummy/standalone-prices/' + id)
+            .send({
+                version: 2,
+                actions: [
+                    {
+                        action: 'setDiscountedPrice',
+                        discounted: null,
+                    },
+                ],
+            })
+
+        expect(response2.status).toBe(200)
+
+        expect(response2.body).toEqual({
+            active: true,
+            channel: {
+                id: 'bar',
+                typeId: 'channel',
+            },
+            country: 'DE',
+            createdAt: expect.anything(),
+            id: id,
+            lastModifiedAt: expect.anything(),
+            sku: 'foo',
+            value: {
+                centAmount: 100,
+                currencyCode: 'EUR',
+                fractionDigits: 2,
+                type: 'centPrecision',
+            },
+            version: 3,
+        })
     })
 })
