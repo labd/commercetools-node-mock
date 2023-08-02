013f83e173195abc1ba8df5f76b42e178c81e5bd
Thomas De Meyer <t.demeyer@labdigital.nl>
2023-08-02 14:42:03 +0200

    added attribute group logic

--- src/repositories/attribute-group.ts
+++ src/repositories/attribute-group.ts
@@ -1,14 +1,58 @@
-import type { AttributeGroup } from '@commercetools/platform-sdk'
-import {
-	AbstractResourceRepository,
-	type RepositoryContext,
-} from './abstract.js'
+import type {
+    AttributeGroup,
+    AttributeGroupChangeNameAction,
+    AttributeGroupDraft,
+    AttributeGroupSetAttributesAction, AttributeGroupSetDescriptionAction, AttributeGroupSetKeyAction
+} from '@commercetools/platform-sdk'
+import {AbstractResourceRepository, type RepositoryContext} from './abstract.js'
+import {getBaseResourceProperties} from '../helpers.js';
+import {Writable} from "../types.js";
 
 export class AttributeGroupRepository extends AbstractResourceRepository<'attribute-group'> {
-	getTypeId() {
-		return 'attribute-group' as const
-	}
-	create(context: RepositoryContext, draft: any): AttributeGroup {
-		throw new Error('Method not implemented.')
-	}
+    getTypeId() {
+        return 'attribute-group' as const
+    }
+
+    create(context: RepositoryContext, draft: AttributeGroupDraft): AttributeGroup {
+        const resource: AttributeGroup = {
+            ...getBaseResourceProperties(),
+            name: draft.name,
+            description: draft.description,
+            key: draft.key,
+            attributes: draft.attributes
+        }
+        this.saveNew(context, resource)
+        return resource
+    }
+
+    actions = {
+        setAttributes: (
+            _context: RepositoryContext,
+            resource: Writable<AttributeGroup>,
+            {attributes}: AttributeGroupSetAttributesAction
+        ) => {
+            resource.attributes = attributes
+        },
+        changeName: (
+            _context: RepositoryContext,
+            resource: Writable<AttributeGroup>,
+            {name}: AttributeGroupChangeNameAction
+        ) => {
+            resource.name = name
+        },
+        setDescription: (
+            _context: RepositoryContext,
+            resource: Writable<AttributeGroup>,
+            {description}: AttributeGroupSetDescriptionAction
+        ) => {
+            resource.description = description
+        },
+        setKey: (
+            _context: RepositoryContext,
+            resource: Writable<AttributeGroup>,
+            {key}: AttributeGroupSetKeyAction
+        ) => {
+            resource.key = key
+        },
+    }
 }
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,79 +1,78 @@
-import { CartService } from './cart.js'
-import { CartDiscountService } from './cart-discount.js'
-import { CategoryServices } from './category.js'
-import { ChannelService } from './channel.js'
-import { CustomObjectService } from './custom-object.js'
-import { CustomerService } from './customer.js'
-import { CustomerGroupService } from './customer-group.js'
-import { DiscountCodeService } from './discount-code.js'
-import { ExtensionServices } from './extension.js'
-import { InventoryEntryService } from './inventory-entry.js'
-import { MyCartService } from './my-cart.js'
-import { MyCustomerService } from './my-customer.js'
-import { MyOrderService } from './my-order.js'
-import { MyPaymentService } from './my-payment.js'
-import { OrderService } from './order.js'
-import { PaymentService } from './payment.js'
-import { ProductService } from './product.js'
-import { ProductDiscountService } from './product-discount.js'
-import { ProductProjectionService } from './product-projection.js'
-import { ProductTypeService } from './product-type.js'
-import { ShippingMethodService } from './shipping-method.js'
-import { ShoppingListService } from './shopping-list.js'
-import { StandAlonePriceService } from './standalone-price.js'
-import { StateService } from './state.js'
-import { StoreService } from './store.js'
-import { SubscriptionService } from './subscription.js'
-import { TaxCategoryService } from './tax-category.js'
-import { TypeService } from './type.js'
-import { ZoneService } from './zone.js'
+import {CartService} from './cart.js'
+import {CartDiscountService} from './cart-discount.js'
+import {CategoryServices} from './category.js'
+import {ChannelService} from './channel.js'
+import {CustomObjectService} from './custom-object.js'
+import {CustomerService} from './customer.js'
+import {CustomerGroupService} from './customer-group.js'
+import {DiscountCodeService} from './discount-code.js'
+import {ExtensionServices} from './extension.js'
+import {InventoryEntryService} from './inventory-entry.js'
+import {MyCartService} from './my-cart.js'
+import {MyCustomerService} from './my-customer.js'
+import {MyOrderService} from './my-order.js'
+import {MyPaymentService} from './my-payment.js'
+import {OrderService} from './order.js'
+import {PaymentService} from './payment.js'
+import {ProductService} from './product.js'
+import {ProductDiscountService} from './product-discount.js'
+import {ProductProjectionService} from './product-projection.js'
+import {ProductTypeService} from './product-type.js'
+import {ShippingMethodService} from './shipping-method.js'
+import {ShoppingListService} from './shopping-list.js'
+import {StandAlonePriceService} from './standalone-price.js'
+import {StateService} from './state.js'
+import {StoreService} from './store.js'
+import {SubscriptionService} from './subscription.js'
+import {TaxCategoryService} from './tax-category.js'
+import {TypeService} from './type.js'
+import {ZoneService} from './zone.js'
+import {AttributeGroupService} from "./attribute-group.js";
 
 export const createServices = (router: any, repos: any) => ({
-	category: new CategoryServices(router, repos['category']),
-	cart: new CartService(router, repos['cart'], repos['order']),
-	'cart-discount': new CartDiscountService(router, repos['cart-discount']),
-	customer: new CustomerService(router, repos['customer']),
-	channel: new ChannelService(router, repos['channel']),
-	'customer-group': new CustomerGroupService(router, repos['customer-group']),
-	'discount-code': new DiscountCodeService(router, repos['discount-code']),
-	extension: new ExtensionServices(router, repos['extension']),
-	'inventory-entry': new InventoryEntryService(
-		router,
-		repos['inventory-entry']
-	),
-	'key-value-document': new CustomObjectService(
-		router,
-		repos['key-value-document']
-	),
-	order: new OrderService(router, repos['order']),
-	payment: new PaymentService(router, repos['payment']),
-	'standalone-price': new StandAlonePriceService(
-		router,
-		repos['standalone-price']
-	),
-	'my-cart': new MyCartService(router, repos['my-cart']),
-	'my-order': new MyOrderService(router, repos['my-order']),
-	'my-customer': new MyCustomerService(router, repos['my-customer']),
-	'my-payment': new MyPaymentService(router, repos['my-payment']),
-	'shipping-method': new ShippingMethodService(
-		router,
-		repos['shipping-method']
-	),
-	'product-type': new ProductTypeService(router, repos['product-type']),
-	product: new ProductService(router, repos['product']),
-	'product-discount': new ProductDiscountService(
-		router,
-		repos['product-discount']
-	),
-	'product-projection': new ProductProjectionService(
-		router,
-		repos['product-projection']
-	),
-	'shopping-list': new ShoppingListService(router, repos['shopping-list']),
-	state: new StateService(router, repos['state']),
-	store: new StoreService(router, repos['store']),
-	subscription: new SubscriptionService(router, repos['subscription']),
-	'tax-category': new TaxCategoryService(router, repos['tax-category']),
-	type: new TypeService(router, repos['type']),
-	zone: new ZoneService(router, repos['zone']),
+    category: new CategoryServices(router, repos['category']),
+    cart: new CartService(router, repos['cart'], repos['order']),
+    'cart-discount': new CartDiscountService(router, repos['cart-discount']),
+    customer: new CustomerService(router, repos['customer']),
+    channel: new ChannelService(router, repos['channel']),
+    'customer-group': new CustomerGroupService(router, repos['customer-group']),
+    'discount-code': new DiscountCodeService(router, repos['discount-code']),
+    extension: new ExtensionServices(router, repos['extension']),
+    'inventory-entry': new InventoryEntryService(
+        router,
+        repos['inventory-entry']
+    ),
+    'key-value-document': new CustomObjectService(
+        router,
+        repos['key-value-document']
+    ),
+    order: new OrderService(router, repos['order']),
+    payment: new PaymentService(router, repos['payment']),
+    'standalone-price': new StandAlonePriceService(router, repos['standalone-price']),
+    'my-cart': new MyCartService(router, repos['my-cart']),
+    'my-order': new MyOrderService(router, repos['my-order']),
+    'my-customer': new MyCustomerService(router, repos['my-customer']),
+    'my-payment': new MyPaymentService(router, repos['my-payment']),
+    'shipping-method': new ShippingMethodService(
+        router,
+        repos['shipping-method']
+    ),
+    'product-type': new ProductTypeService(router, repos['product-type']),
+    product: new ProductService(router, repos['product']),
+    'product-discount': new ProductDiscountService(
+        router,
+        repos['product-discount']
+    ),
+    'product-projection': new ProductProjectionService(
+        router,
+        repos['product-projection']
+    ),
+    'shopping-list': new ShoppingListService(router, repos['shopping-list']),
+    state: new StateService(router, repos['state']),
+    store: new StoreService(router, repos['store']),
+    subscription: new SubscriptionService(router, repos['subscription']),
+    'tax-category': new TaxCategoryService(router, repos['tax-category']),
+    'attribute-group': new AttributeGroupService(router, repos['attribute-group']),
+    type: new TypeService(router, repos['type']),
+    zone: new ZoneService(router, repos['zone']),
 })
