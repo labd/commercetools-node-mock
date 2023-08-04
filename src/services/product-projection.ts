957864da3ddd35eba66ff3db6abac6b2d11604e3
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-08-04 14:09:27 +0200

    Fix the product projection query endpoint
    This didn’t work at all previously. This implementation should make the
    staged/non-staged work correctly and should include proper filtering.
    
    Specific storeProjection or localeProjections are not supported yet

--- src/services/product-projection.ts
+++ src/services/product-projection.ts
@@ -19,6 +19,20 @@ export class ProductProjectionService extends AbstractService {
 		router.get('/search', this.search.bind(this))
 	}
 
+	get(request: Request, response: Response) {
+		const limit = this._parseParam(request.query.limit)
+		const offset = this._parseParam(request.query.offset)
+
+		const result = this.repository.query(getRepositoryContext(request), {
+			...request.query,
+			expand: this._parseParam(request.query.expand),
+			where: this._parseParam(request.query.where),
+			limit: limit !== undefined ? Number(limit) : undefined,
+			offset: offset !== undefined ? Number(offset) : undefined,
+		})
+		return response.status(200).send(result)
+	}
+
 	search(request: Request, response: Response) {
 		const resource = this.repository.search(
 			getRepositoryContext(request),
