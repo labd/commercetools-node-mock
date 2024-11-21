labd/commercetools-node-mock

commit b145e3db4f6bd6d5213f83584d14d5b185578004

fix: formatting

src/repositories/customer/index.test.ts
@@ -1,12 +1,11 @@
+import type { Store } from "@commercetools/platform-sdk";
 import { describe, expect, test } from "vitest";
 import { InMemoryStorage } from "~src/storage";
 import { CustomerRepository } from "./index";
-import { Store } from "@commercetools/platform-sdk";
 
 describe("Order repository", () => {
 	const storage = new InMemoryStorage();
 	const repository = new CustomerRepository(storage);
-
 
 	test("adding stores to customer", async () => {
 		const store1: Store = {
@@ -48,14 +47,16 @@ describe("Order repository", () => {
 			{ projectKey: "dummy" },
 			{
 				email: "my-customer@email.com",
-				stores: [{
-					typeId: "store",
-					id: store1.id
-				},
-				{
-					typeId: "store",
-					key: store2.key
-				}]
+				stores: [
+					{
+						typeId: "store",
+						id: store1.id,
+					},
+					{
+						typeId: "store",
+						key: store2.key,
+					},
+				],
 			},
 		);
 
@@ -63,11 +64,11 @@ describe("Order repository", () => {
 		expect(result?.stores).toEqual([
 			{
 				typeId: "store",
-				key: store1.key
+				key: store1.key,
 			},
 			{
 				typeId: "store",
-				key: store2.key
+				key: store2.key,
 			},
 		]);
 	});

commit f61e38dc32af4094c49618354c4ea393d35baaab

fix(customer): customer repository allow no stores (#251)
Empty arrays for the stores of a customer were wrongfully check for
empty arrays. Fixes issue introduced in #249.

src/repositories/customer/index.ts
@@ -105,7 +105,7 @@ export class CustomerRepository extends AbstractResourceRepository<"customer"> {
 
 		let storesForCustomer: StoreKeyReference[] = [];
 
-		if (draft.stores) {
+		if (draft.stores && draft.stores.length > 0) {
 			const storeIds = draft.stores
 				.map((storeReference) => storeReference.id)
 				.filter(Boolean);

src/repositories/customer/index.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, test } from "vitest";
 import { InMemoryStorage } from "~src/storage";
 import { CustomerRepository } from "./index";
 
-describe("Order repository", () => {
+describe("Customer repository", () => {
 	const storage = new InMemoryStorage();
 	const repository = new CustomerRepository(storage);
 
@@ -110,5 +110,18 @@ describe("Order repository", () => {
 				key: store2.key,
 			},
 		]);
+	});
+
+	test("adding customer without linked stores", async () => {
+		const result = repository.create(
+			{ projectKey: "dummy" },
+			{
+				email: "my-customer-without-stores@email.com",
+				stores: [],
+			},
+		);
+
+		expect(result.email).toEqual("my-customer-without-stores@email.com");
+		expect(result?.stores).toHaveLength(0);
 	});
 });