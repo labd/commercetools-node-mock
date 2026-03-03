591889359f8bf207901c30406145bbf994944f3c
Michael van Tellingen <m.vantellingen@labdigital.nl>
2026-03-03 12:34:34 +0100

    chore: split sqlite backend dist file
    This fixes the issue that node < 24 has no sqlite module

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
 import type { CommercetoolsMockOptions } from "./ctMock.ts";
 import { CommercetoolsMock } from "./ctMock.ts";
 import { getBaseResourceProperties } from "./helpers.ts";
-import {
-	AbstractStorage,
-	InMemoryStorage,
-	SQLiteStorage,
-} from "./storage/index.ts";
+import { AbstractStorage, InMemoryStorage } from "./storage/index.ts";
 
 export {
 	AbstractStorage,
 	CommercetoolsMock,
 	InMemoryStorage,
-	SQLiteStorage,
 	getBaseResourceProperties,
 	type CommercetoolsMockOptions,
 };
